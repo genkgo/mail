3b7e55e67b8278381834eafa8657bac4bc6da1d6
Frederik Bosch <f.bosch@genkgo.nl>
2017-05-24 15:07:03 +0200

    add inject date transport

---
+++ src/Transport/InjectDateHeaderTransport.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Genkgo\Mail\Transport;
+
+use Genkgo\Mail\Header\Date;
+use Genkgo\Mail\MessageInterface;
+use Genkgo\Mail\TransportInterface;
+
+final class InjectDateHeaderTransport implements TransportInterface
+{
+    /**
+     * @var TransportInterface
+     */
+    private $decoratedTransport;
+
+    /**
+     * InjectDateHeaderTransport constructor.
+     * @param TransportInterface $transport
+     */
+    public function __construct(TransportInterface $transport)
+    {
+        $this->decoratedTransport = $transport;
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @return void
+     */
+    public function send(MessageInterface $message): void
+    {
+        $this->decoratedTransport->send(
+            $message->withHeader(
+                new Date(
+                    new \DateTimeImmutable()
+                )
+            )
+        );
+    }
+}
---
+++ test/Unit/Transport/InjectDateHeaderTransportTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Genkgo\Mail\Unit\Transport;
+
+use Genkgo\Mail\AbstractTestCase;
+use Genkgo\Mail\GenericMessage;
+use Genkgo\Mail\Header\Date;
+use Genkgo\Mail\Transport\ArrayObjectTransport;
+use Genkgo\Mail\Transport\InjectDateHeaderTransport;
+
+final class InjectDateHeaderTransportTest extends AbstractTestCase
+{
+    /**
+     * @test
+     */
+    public function it_injects_date_header_in_message()
+    {
+        $message = new GenericMessage();
+        $storage = new \ArrayObject();
+
+        $transport = new InjectDateHeaderTransport(
+            new ArrayObjectTransport($storage)
+        );
+        $transport->send($message);
+
+        $this->assertCount(1, $storage);
+        $this->assertTrue($storage[0]->hasHeader('date'));
+        $this->assertInstanceOf(Date::class, $storage[0]->getHeader('date')[0]);
+    }
+
+}
