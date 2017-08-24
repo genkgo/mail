3adfc89971b5cdd5f0110124f28c57c2036f7aa4
Frederik Bosch <f.bosch@genkgo.nl>
2017-08-24 16:17:29 +0200

    only inject a message id when there is none

--- test/Unit/Transport/InjectMessageIdHeaderTransportTest.php
+++ test/Unit/Transport/InjectMessageIdHeaderTransportTest.php
@@ -3,6 +3,7 @@
 namespace Genkgo\TestMail\Unit\Transport;
 
 use Genkgo\Mail\Header\MessageId;
+use Genkgo\Mail\HeaderInterface;
 use Genkgo\Mail\Transport\InjectMessageIdHeaderTransport;
 use Genkgo\TestMail\AbstractTestCase;;
 use Genkgo\Mail\GenericMessage;
@@ -27,6 +28,31 @@ final class InjectMessageIdHeaderTransportTest extends AbstractTestCase
         $this->assertCount(1, $storage);
         $this->assertTrue($storage[0]->hasHeader('message-id'));
         $this->assertInstanceOf(MessageId::class, $storage[0]->getHeader('message-id')[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_inject_a_message_id_header_when_there_is_one()
+    {
+        $message = (new GenericMessage())
+            ->withHeader(new MessageId('left', 'right'));
+
+        $storage = new \ArrayObject();
+
+        $transport = new InjectMessageIdHeaderTransport(
+            new ArrayObjectTransport($storage),
+            'domain'
+        );
+        $transport->send($message);
+
+        $this->assertCount(1, $storage);
+        $this->assertTrue($storage[0]->hasHeader('message-id'));
+        $this->assertInstanceOf(MessageId::class, $storage[0]->getHeader('message-id')[0]);
+
+        /** @var HeaderInterface $header */
+        $header = $storage[0]->getHeader('message-id')[0];
+        $this->assertEquals('<left@right>', (string)$header->getValue());
     }
 
 }
