e5883551c4e276c34d2f84345865869ac54c2ad4
Frederik Bosch <f.bosch@genkgo.nl>
2020-09-17 13:02:47 +0200

    add imap move account (#77)

---
+++ src/Protocol/Imap/Request/MoveCommand.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Genkgo\Mail\Protocol\Imap\Request;
+
+use Genkgo\Mail\Protocol\Imap\MailboxName;
+use Genkgo\Mail\Protocol\Imap\Tag;
+use Genkgo\Mail\Stream\StringStream;
+use Genkgo\Mail\StreamInterface;
+
+final class MoveCommand extends AbstractCommand
+{
+    /**
+     * @var SequenceSet
+     */
+    private $set;
+
+    /**
+     * @var Tag
+     */
+    private $tag;
+
+    /**
+     * @var MailboxName
+     */
+    private $mailbox;
+
+    /**
+     * @param Tag $tag
+     * @param SequenceSet $set
+     * @param MailboxName $mailbox
+     */
+    public function __construct(Tag $tag, SequenceSet $set, MailboxName $mailbox)
+    {
+        $this->set = $set;
+        $this->tag = $tag;
+        $this->mailbox = $mailbox;
+    }
+
+    /**
+     * @return StreamInterface
+     */
+    protected function createStream(): StreamInterface
+    {
+        return new StringStream(
+            \sprintf(
+                'MOVE %s %s',
+                (string)$this->set,
+                (string)$this->mailbox
+            )
+        );
+    }
+
+    /**
+     * @return Tag
+     */
+    public function getTag(): Tag
+    {
+        return $this->tag;
+    }
+}
