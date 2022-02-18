22a5b827b2a13c18158b1dbf87746986d27c665d
Frederik Bosch <f.bosch@genkgo.nl>
2022-02-18 17:11:35 +0100

    upgrade php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,11 @@
 <?php
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
         'declare_strict_types' => true,
         'yoda_style' => false,
-        'native_function_invocation' => true,
+        'native_function_invocation' => ['include' => ['@all']],
         'phpdoc_no_package' => true,
         'no_empty_phpdoc' => true,
         'no_blank_lines_after_class_opening' => true,
